2e043ca94baa409d3e1bfe6359484974c76d29f2
Rikikangsc2-eng <nueapi11@gmail.com>
2025-05-31 20:07:07 +0700

    update ig-post-generator.js

--- API/Image/ig-post-generator.js
+++ API/Image/ig-post-generator.js
@@ -1,44 +1,36 @@
 const axios = require('axios');
-const querystring = require('querystring');
 
 module.exports = async function(req, res) {
   try {
-    const paramsDariLu = req.query;
+    const kueriPengguna = req.query;
+    const stringKueri = new URLSearchParams(kueriPengguna).toString();
+    const urlTujuan = `https://nirkyy-api.hf.space/api/fis?${stringKueri}`;
     
-    if (Object.keys(paramsDariLu).length === 0) {
-      return res.errorJson("Waduh, parameternya kosong nih. Coba isi dulu ya.", 400);
-    }
-    
-    const urlTarget = `https://nirkyy-api.hf.space/api/fis?${querystring.stringify(paramsDariLu)}`;
-    
-    const responDariApi = await axios.get(urlTarget, {
+    const responDariLuar = await axios({
+      method: 'get',
+      url: urlTujuan,
       responseType: 'stream'
     });
     
-    if (responDariApi.headers['content-type'] && responDariApi.headers['content-type'].includes('image/jpeg')) {
-      res.setHeader('Content-Type', 'image/jpeg');
-      responDariApi.data.pipe(res);
-    } else {
-      let pesanErrorTambahan = "Kayaknya API tujuan gak ngasih gambar JPEG deh.";
-      if (responDariApi.headers['content-type']) {
-        pesanErrorTambahan += ` Malah ngasih: ${responDariApi.headers['content-type']}`;
-      }
-      return res.errorJson(`Ada yang aneh nih sama respons dari API. ${pesanErrorTambahan}`, 502);
-    }
+    res.setHeader('Content-Type', responDariLuar.headers['content-type'] || 'image/jpeg');
+    responDariLuar.data.pipe(res);
     
-  } catch (e) {
-    if (e.response) {
-      let pesanErrorDetail = `Server API tujuan ngambek nih, statusnya ${e.response.status}.`;
-      if (e.response.data && typeof e.response.data === 'object') {
-        pesanErrorDetail += ` Pesannya: ${JSON.stringify(e.response.data)}`;
-      } else if (e.response.data) {
-        pesanErrorDetail += ` Pesannya: ${e.response.data}`;
-      }
-      return res.errorJson(pesanErrorDetail, e.response.status || 500);
-    } else if (e.request) {
-      return res.errorJson("Duh, gagal nyambung ke API tujuan nih. Cek koneksi atau URL-nya coba.", 503);
+  } catch (eror) {
+    if (eror.response) {
+      res.errorJson(
+        `Waduh, kayaknya API yang kamu tuju lagi ngambek nih, ngasih status ${eror.response.status}. Pesan dari sononya sih: '${eror.response.statusText || 'Gak ada pesan khusus.'}' Coba cek lagi inputanmu atau mungkin API-nya lagi ada gangguan.`,
+        eror.response.status
+      );
+    } else if (eror.request) {
+      res.errorJson(
+        'Bro, gagal total nih nyambung ke API tujuan. Cek koneksi internetmu deh, atau mungkin alamat API-nya lagi gak bisa diakses sama sekali. Sabar ya, coba lagi nanti.',
+        503
+      );
     } else {
-      return res.errorJson(`Waduh, ada yang error nih pas mau ngambil data: ${e.message}. Coba lagi nanti ya.`, 500);
+      res.errorJson(
+        `Yah, ada yang aneh nih pas mau ngolah permintaanmu di server kami. Maaf banget ya. Detail errornya: ${eror.message}. Coba lagi aja beberapa saat lagi.`,
+        500
+      );
     }
   }
 };
